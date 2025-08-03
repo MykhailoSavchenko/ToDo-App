f68dbcf3d351e6b3f274b982cdaaf4370f0c6d64
Mykhailo Savchenko <savchenko.mihail02@gmail.com>
2025-08-03 17:49:51 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,99 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
+import * as todosApi from './api/todos';
+import { useTodoActions } from './hooks/useTodoActions';
+import { prepareTodoList } from './utils/preparedTodoList';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { FilterParams, Todo } from './types/types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { AppHeader } from './components/Header';
+import { TodoItem } from './components/TodoItem';
+import { AppFooter } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const {
+    isAllTodoCompleted,
+    isCompletedTodos,
+    todoInOperation,
+    errorMessage,
+    activeTodos,
+    isLoading,
+    tempTodo,
+    todoData,
+    hasTodo,
+    setErrorMessage,
+    deleteCompleted,
+    handleUpdate,
+    deleteTodo,
+    toggleTodo,
+    toggleAll,
+    addTodo,
+  } = useTodoActions();
+
+  const [filter, setFilter] = useState<FilterParams>(FilterParams.All);
+
+  const todoList = prepareTodoList(todoData, filter);
+
+  if (!todosApi.USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <AppHeader
+          isAllTodoCompleted={isAllTodoCompleted}
+          isLoading={isLoading}
+          hasTodo={hasTodo}
+          toggleAll={toggleAll}
+          addTodo={addTodo}
+        />
+
+        <section className="todoapp__main" data-cy="TodoList">
+          <TransitionGroup>
+            {todoList.map((todo: Todo) => {
+              const isOverlayActive = todoInOperation.includes(todo.id);
+
+              return (
+                <CSSTransition key={todo.id} timeout={300} classNames="item">
+                  <TodoItem
+                    isOverlayActive={isOverlayActive}
+                    todo={todo}
+                    handleUpdate={handleUpdate}
+                    deleteTodo={deleteTodo}
+                    toggleTodo={toggleTodo}
+                  />
+                </CSSTransition>
+              );
+            })}
+
+            {tempTodo && (
+              <CSSTransition key={0} timeout={300} classNames="temp-item">
+                <TodoItem todo={tempTodo} />
+              </CSSTransition>
+            )}
+          </TransitionGroup>
+        </section>
+
+        {hasTodo && (
+          <AppFooter
+            isCompletedTodos={isCompletedTodos}
+            filter={filter}
+            activeTodos={activeTodos}
+            deleteCompleted={deleteCompleted}
+            setFilter={setFilter}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      />
     </div>
   );
 };
